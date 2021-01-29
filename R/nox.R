e384f62145bdcdbcf63959e69c5d13b1dbc4de53
guidofioravanti <guidofioravanti@yahoo.it>
2021-01-29 16:12:06 +0100

    Dati no2, nox, pm10, pm25

---
+++ R/nox.R
@@ -0,0 +1,7 @@
+#' Tibble con i dati di nox per 2 stazioni della regione BASILICATA
+#'
+#' @format Un tibble con 8 colonne e 5414 osservazioni
+#'
+#' @usage
+#' nox
+"nox"
---
+++ R/pm25.R
@@ -0,0 +1,7 @@
+#' Tibble con i dati di pm25 per 1 stazioni della regione BASILICATA
+#'
+#' @format Un tibble con 8 colonne e 2675 osservazioni
+#'
+#' @usage
+#' pm25
+"pm25"
